6149f6ad7350c6665d5841075e3587520881fbd8
Torsten Mandry <torsten.mandry@innoq.com>
2019-10-24 17:28:39 +0200

    replace OPS with Esri satellite map

--- index.js
+++ index.js
@@ -4,14 +4,20 @@ import View from "ol/View";
 import { fromLonLat } from "ol/proj";
 import { Draw, Modify, Snap } from "ol/interaction";
 import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer";
-import { OSM, Vector as VectorSource } from "ol/source";
+import { XYZ, Vector as VectorSource } from "ol/source";
 import { Circle as CircleStyle, Fill, Stroke, Style } from "ol/style";
 
 let currentPosition = [0, 0];
 let polygonPoints = [];
 
 const raster = new TileLayer({
-  source: new OSM()
+  source: new XYZ({
+    attributions: ['Powered by Esri',
+                   'Source: Esri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AeroGRID, IGN, and the GIS User Community'],
+    attributionsCollapsible: false,
+    url: 'https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+    maxZoom: 23
+  })
 });
 
 const source = new VectorSource();
